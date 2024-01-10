04c5093e7f2cc97fee6a0d8b7d1917e53fd48e2c
Alina <wbrflf3301@outlook.com>
2024-01-10 16:35:17 +0500

    Added MainFragment

---
+++ app/src/main/java/com/example/kotlin_wordle/MainFragment.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_wordle
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.kotlin_wordle.databinding.FragmentMainBinding
+
+class SampleFragment : Fragment(R.layout.fragment_main) {
+
+    private val binding: FragmentMainBinding by viewBinding()
+
+}
